0bad5e279d87fd1848f5eb32364847d4bed8a71d
Hikmabalqis <balqishikmatullah15@gmail.com>
2023-06-15 17:32:28 +0700

    update

--- src/routes/productRouter.ts
+++ src/routes/productRouter.ts
@@ -112,4 +112,36 @@ router.delete('/:id', async (req: Request, res: Response) => {
     }
 });
 
+router.get('/owner/:ownerId/:page/:limit', async (req: Request, res: Response) => {
+    try {
+      const ownerId = req.params.ownerId;
+      const page = parseInt(req.params.page) || 1; // Halaman saat ini
+      const limit = parseInt(req.params.limit) || 10; // Jumlah produk per halaman
+  
+      const skip = (page - 1) * limit;
+  
+      const countPromise = ProductModel.countDocuments({ owner: ownerId }).exec();
+      const productsPromise = ProductModel.find({ owner: ownerId })
+        .skip(skip)
+        .limit(limit)
+        .exec();
+  
+      const [count, products] = await Promise.all([countPromise, productsPromise]);
+  
+      const totalPages = Math.ceil(count / limit);
+  
+      res.json({
+        page,
+        limit,
+        totalProducts: count,
+        totalPages,
+        products,
+      });
+    } catch (error) {
+      res.status(500).json({ message: 'Terjadi kesalahan saat mengambil produk berdasarkan pemilik.', error });
+    }
+  });
+  
+
 export default router;
+
